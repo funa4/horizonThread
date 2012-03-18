f001c1dc2b837f2bb11e687f42061d8fa54d51e3
久保 隆宏 <kubo20@114-134-195-201.fnnr.j-cnet.jp>
2012-03-18 15:33:22 +0900

    fix environmental port

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express')
   , routes = require('./routes');
 
 var app = module.exports = express.createServer();
+var port = process.env.PORT || 3000; //for heroku
 
 // Configuration
 
@@ -31,5 +32,5 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
