a0aac26a954a9b1423cef868c6ae093291b6ff3d
久保 隆宏 <kubo20@114-134-195-201.fnnr.j-cnet.jp>
2012-03-19 22:50:21 +0900

    init

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ var port = process.env.PORT || 3000; //for heroku
  //extends connect function
  mongoose.connectDB = function(){
  	console.log("db connec by " + db_uri);
+ 	if(db.connections[0].readyState == 2){//connecting now
+ 		console.log("connecting is alive. disclose now ... ");
+ 		db.disconnect();
+ 	}
 	this.connect(db_uri,
 		function(err){
 			if(err){
